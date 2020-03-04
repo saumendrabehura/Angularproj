5bb8b5bbf3ed8853b728818b827a4e09731eb04a
saumendra <saumendra08@gmail.com>
2020-03-04 07:12:06 +0530

    First message

---
+++ SprinAngular-4/src/main/java/com/angular/SprinAngular4Application.java
@@ -0,0 +1,13 @@
+package com.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinAngular4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinAngular4Application.class, args);
+	}
+
+}
---
+++ SprinAngular-4/src/test/java/com/angular/SprinAngular4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinAngular4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
